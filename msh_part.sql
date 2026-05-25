ff9565e7745347a951d964066fc5378bad74aef8
Your Name <mahshid.sheybani@email.kntu.ac.ir>
2026-05-26 00:40:06 +0330

    Add User table

--- msh_part.sql
+++ msh_part.sql
@@ -12,3 +12,21 @@ CREATE TABLE Team (
     name VARCHAR(100) NOT NULL
 );
 
+CREATE TABLE User (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    phone VARCHAR(15) NOT NULL UNIQUE,
+    role VARCHAR(20) NOT NULL,
+    city_id INT,
+    password_hash VARCHAR(255) NOT NULL,
+    registered_at TIMESTAMP NOT NULL,
+    account_status VARCHAR(10) NOT NULL,
+    profile_picture VARCHAR(255),
+    FOREIGN KEY (city_id)
+        REFERENCES City (city_id)
+);
+
+
+
