204ca31354a3751bba96d38805639122eb2600f6
Your Name <mahshid.sheybani@email.kntu.ac.ir>
2026-05-27 02:25:50 +0330

    Add sample date inserts

--- msh_part.sql
+++ msh_part.sql
@@ -28,3 +28,53 @@ CREATE TABLE User (
         REFERENCES City (city_id)
         ON DELETE SET NULL
 );
+
+INSERT INTO City (city_id, name, province)
+VALUES
+(1, 'Tehran', 'Tehran'),
+(2, 'Mashhad', 'Razavi Khorasan'),
+(3, 'Isfahan', 'Isfahan'),
+(4, 'Shiraz', 'Fars'),
+(5, 'Rasht', 'Gilan');
+
+INSERT INTO Team (team_id, name)
+VALUES
+(1, 'Perspolis'),
+(2, 'Esteghlal'),
+(3, 'Sepahan'),
+(4, 'Tractor'),
+(5, 'Foolad');
+
+INSERT INTO User
+(user_id, first_name, last_name, email, phone, role, city_id, password_hash, account_status)
+VALUES
+
+(1, 'Ali', 'Ahmadi', 'ali@gmail.com', '09126412825', 'spectator', 1, 'hashed_pass_1', 'active'),
+
+(2, 'Aynaz', 'Hosseini', 'aynaz@gmail.com', '09151270125', 'admin', 2, 'hashed_pass_2', 'active'),
+
+(3, 'Reza', 'Karimi', 'reza@gmail.com', '09012379854', 'spectator', 3, 'hashed_pass_3', 'inactive'),
+
+(4, 'Nika', 'Jafari',
+'nika@gmail.com',
+'09010105123',
+'spectator',
+4,
+'hashed_pass_4',
+'active'),
+
+(5, 'Parsa', 'Hosseini',
+'parsa@gmail.com',
+'09031348732',
+'admin',
+5,
+'hashed_pass_5',
+'active'),
+
+(6, 'Yasamin', 'adib',
+'yasamin@gmail.com',
+'09052456630',
+'spectator',
+1,
+'hashed_pass_6',
+'active');
