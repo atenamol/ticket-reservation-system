ffec13a6152571ceb4887f61c8a7ead9b6c402df
Zahra Sarvari <zahrasarvariqq@gmail.com>
2026-05-26 10:14:33 +0330

    add Match table

--- SQL/zs_part_CreateTableSql.sql
+++ SQL/zs_part_CreateTableSql.sql
@@ -10,3 +10,15 @@ CREATE TABLE Venue (
     refund_policy_rules TEXT,
     FOREIGN KEY (city_id) REFERENCES City(city_id)
 );
+
+CREATE TABLE `Match` (
+    match_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    sport_type VARCHAR(50),
+    home_team_id INT(11),
+    away_team_id INT(11),
+    venue_id INT(11) NOT NULL,
+    match_date DATETIME,
+    FOREIGN KEY (venue_id) REFERENCES Venue(venue_id) ON DELETE SET NULL,
+    FOREIGN KEY (home_team_id) REFERENCES Team(team_id),
+    FOREIGN KEY (away_team_id) REFERENCES Team(team_id)
+);
