c93ecde4f62791bf7ee17744d31b41fb37cb8fe8
Zahra Sarvari <zahrasarvariqq@gmail.com>
2026-05-26 11:21:29 +0330

    add Ticket table

--- SQL/zs_part_CreateTableSql.sql
+++ SQL/zs_part_CreateTableSql.sql
@@ -11,7 +11,7 @@ CREATE TABLE Venue (
     FOREIGN KEY (city_id) REFERENCES City(city_id)
 );
 
-CREATE TABLE `Match` (
+CREATE TABLE Matchh (
     match_id INT(11) PRIMARY KEY AUTO_INCREMENT,
     sport_type VARCHAR(50),
     home_team_id INT(11),
@@ -22,3 +22,15 @@ CREATE TABLE `Match` (
     FOREIGN KEY (home_team_id) REFERENCES Team(team_id),
     FOREIGN KEY (away_team_id) REFERENCES Team(team_id)
 );
+
+CREATE TABLE Ticket (
+    ticket_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    match_id INT(11) NOT NULL,
+    price DECIMAL(10, 2) CHECK (price >= 0),
+    remaining_capacity INT(11) CHECK (remaining_capacity >= 0),
+    category ENUM('VIP', 'normal', 'economy') NOT NULL,
+    organizer_venue_id INT(11), 
+    FOREIGN KEY (match_id) REFERENCES Matchh(match_id) ON DELETE CASCADE,
+    FOREIGN KEY (organizer_venue_id) REFERENCES Venue(venue_id)
+);
+
