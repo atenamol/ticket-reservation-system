a2c36d9f0263340a6548d406c1d3944569ec52a3
mahshid sheybani <mahshidsheybani2825@gmail.com>
2026-07-20 19:59:38 +0330

    add query 4, 5, 6 and 7

--- AllQueries.sql
+++ AllQueries.sql
@@ -4,9 +4,7 @@ USE TicketSystem;
 SELECT 
     u.user_id,
     u.first_name,
-    u.last_name,
-    u.email,
-    u.phone
+    u.last_name
 FROM User u
 LEFT JOIN Reservation r ON u.user_id = r.user_id
 WHERE r.reservation_id IS NULL;
@@ -15,9 +13,7 @@ WHERE r.reservation_id IS NULL;
 SELECT DISTINCT
     u.user_id,
     u.first_name,
-    u.last_name,
-    u.email,
-    u.phone
+    u.last_name
 FROM User u
 JOIN Reservation r ON u.user_id = r.user_id
 WHERE r.status = 'paid';
@@ -40,3 +36,97 @@ GROUP BY
     YEAR(p.transaction_date),
     MONTH(p.transaction_date)
 ORDER BY year DESC, month DESC, total_paid DESC;
+
+-- QUERY 4: Users who have purchased exactly once in each city
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    c.name AS city_name,
+    COUNT(r.reservation_id) AS purchase_count
+FROM User u
+JOIN Reservation r ON u.user_id = r.user_id
+JOIN Ticket t ON r.ticket_id = t.ticket_id
+JOIN Matchh m ON t.match_id = m.match_id
+JOIN Venue v ON m.venue_id = v.venue_id
+JOIN City c ON v.city_id = c.city_id
+WHERE r.status = 'paid'
+GROUP BY
+	u.user_id,
+    u.first_name,
+    u.last_name,
+    c.city_id,
+    c.name
+HAVING COUNT(r.reservation_id) = 1
+ORDER BY u.user_id, c.name;
+
+-- QUERY 5: User who purchased the most recent ticket
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    u.phone,
+    t.ticket_id,
+    t.price,
+    r.reserved_at AS purchase_time
+FROM User u
+JOIN Reservation r ON u.user_id = r.user_id
+JOIN Ticket t ON r.ticket_id = t.ticket_id
+WHERE r.status = 'paid'
+	AND r.reserved_at = (
+		  SELECT MAX(reserved_at)
+		  FROM Reservation
+		  WHERE status = 'paid'
+	  )
+ORDER BY r.reserved_at DESC
+LIMIT 1;
+
+-- QUERY 6: Users whose total payments exceed the overall average
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    u.phone,
+    SUM(p.amount) AS total_paid,
+    (
+        SELECT AVG(total)
+        FROM (
+            SELECT SUM(amount) AS total
+            FROM Payment
+            WHERE payment_status = 'completed'
+            GROUP BY user_id
+        ) AS avg_table
+    ) AS average_payment
+FROM User u
+JOIN Payment p ON u.user_id = p.user_id
+WHERE p.payment_status = 'completed'
+GROUP BY 
+	u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    u.phone
+HAVING SUM(p.amount) > (
+    SELECT AVG(total) FROM (
+        SELECT SUM(amount) AS total
+        FROM Payment
+        WHERE payment_status = 'completed'
+        GROUP BY user_id
+    ) AS avg_table
+)
+ORDER BY total_paid DESC;
+
+-- QUERY 7: Number of tickets sold per sport type
+SELECT 
+    m.sport_type,
+    COUNT(r.reservation_id) AS tickets_sold
+FROM Matchh m
+LEFT JOIN Ticket t
+    ON m.match_id = t.match_id
+LEFT JOIN Reservation r
+    ON t.ticket_id = r.ticket_id
+    AND r.status = 'paid'
+GROUP BY m.sport_type
+ORDER BY tickets_sold DESC;
