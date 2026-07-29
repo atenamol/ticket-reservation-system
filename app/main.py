3fb202c7b31ebf857e79c2b51527041366c86fc8
Zahra Sarvari <zahrasarvariqq@gmail.com>
2026-07-29 14:35:44 +0330

    implement authentication routes

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from app.routers.transaction_router import router as transaction_router
 from app.routers.catalog_router import router as catalog_router
+from app.routers.auth_router import router as auth_router
 
 app = FastAPI(
     title="Ticket Reservation System",
@@ -10,7 +11,7 @@ app = FastAPI(
 
 app.include_router(transaction_router)
 app.include_router(catalog_router)
-
+app.include_router(auth_router)
 
 @app.get("/")
 def root():
---
+++ app/routers/auth_router.py
@@ -0,0 +1,70 @@
+from typing import Any
+
+from fastapi import APIRouter, Depends
+
+from app.auth.dependencies import get_current_user
+from app.schemas.auth_schema import (
+    SignupRequest,
+    PasswordLoginRequest,
+    OTPLoginRequest,
+    VerifyOTPRequest,
+    UpdateProfileRequest,
+    TokenResponse,
+    UserResponse,
+    MessageResponse,
+)
+
+from app.services.auth_service import (
+    signup,
+    login_with_password,
+    login_with_otp,
+    verify_login_otp,
+    update_profile,
+)
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["Authentication"],
+)
+
+
+@router.post("/signup", response_model=TokenResponse, status_code=201,
+            summary="Register a new user")
+def signup_route(request: SignupRequest):
+
+    token, _ = signup(request)
+
+    return token
+
+
+@router.post("/login/password", response_model=TokenResponse, status_code=200,
+            summary="Login using password")
+def login_password_route(request: PasswordLoginRequest):
+
+    token, _ = login_with_password(request)
+
+    return token
+
+
+@router.post("/login/otp", response_model=MessageResponse, status_code=200, 
+            summary="Send OTP")
+def login_otp_route(request: OTPLoginRequest):
+
+    return login_with_otp(request)
+
+
+@router.post("/verify-otp", response_model=TokenResponse, status_code=200, 
+            summary="Verify OTP")
+def verify_otp_route(request: VerifyOTPRequest):
+
+    token, _ = verify_login_otp(request)
+
+    return token
+
+
+@router.put("/profile", response_model=UserResponse, status_code=200, 
+            summary="Update profile")
+def update_profile_route(request: UpdateProfileRequest, 
+                        current_user: dict[str, Any] = Depends(get_current_user)):
+
+    return update_profile(user_id=current_user["user_id"], request=request)
