84546a3bf567609e734ce38dd52341b3d3fea6c6
atena <molaeeatena603@gmail.com>
2026-08-07 20:07:13 +0330

    Configure CORS for WebStorm and VS Code local servers

--- app/main.py
+++ app/main.py
@@ -10,11 +10,14 @@ app = FastAPI(
     version="1.0.0",
 )
 
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
+        "http://localhost:63342",
+        "http://127.0.0.1:63342"
     ],
     allow_credentials=True,
     allow_methods=["*"],
